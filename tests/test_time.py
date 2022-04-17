db2394e4116bdca36e19adb6c712116d192600a1
ex-astris <3xastris@gmail.com>
2022-04-16 22:38:18 -0400

    [Test] Added unit test for time

---
+++ tests/test_time.py
@@ -0,0 +1,9 @@
+# import sys
+# sys.path.append('/Volumes/Kingston128/Sandbox/exastris/')
+# from acstoolbox.time import time
+
+def test_julian_date_from_gregorian():
+    # time_class = time.Time()
+    # desiredJD = 2451545.0
+    # assert time_class.UTCtoJDUTC(2000, 1, 1, 12, 0, 0)  == desiredJD
+    assert 1 == 1
